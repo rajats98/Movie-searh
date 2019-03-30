c3d66c54c2f3f83313e6f9872944e988e87508bf
Rajat <rajats1.kv2srg@gmail.com>
2019-03-30 12:46:01 +0530

    Done

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var request =require('request');
 var parse = require('parse');
 app.set("view engine","ejs");
 
-
+app.get("/",function(req,res){
+	res.render('home');
+}); 
 app.get("/results",function(req,res){
-
-	request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb",function(error,response,body){
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?s="+query;
+	request(url,function(error,response,body){
 		if(!error && response.statusCode==200){
+
 			var details = JSON.parse(body);
 			res.render('results',{details:details});
 		}
